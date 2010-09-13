296b03484b6c64008d7480bc9cb479dbea2ff501
ornicar <thibault.duplessis@gmail.com>
2010-09-12 22:35:19 -0700

    Switch coding standarts to PEAR conventions, improve phpDoc

--- lib/phpGitRepo.php
+++ lib/phpGitRepo.php
@@ -18,116 +18,115 @@ require_once(dirname(__FILE__).'/phpGitRepoCommand.php');
  */
 class phpGitRepo
 {
-  /**
-   * @var string  local repository directory
-   */
-  protected $dir;
+    /**
+     * @var string  local repository directory
+     */
+    protected $dir;
 
-  /**
-   * @var boolean Whether to enable debug mode or not
-   * When debug mode is on, commands and their output are displayed
-   */
-  protected $debug;
+    /**
+     * @var boolean Whether to enable debug mode or not
+     * When debug mode is on, commands and their output are displayed
+     */
+    protected $debug;
 
-  /**
-   * @var array of options
-   */
-  protected $options = array(
-    'command_class'   => 'phpGitRepoCommand', // class used to create a command
-    'git_executable'  => '/usr/bin/git'       // path of the executable on the server
-  );
+    /**
+     * @var array of options
+     */
+    protected $options = array(
+        'command_class'   => 'phpGitRepoCommand', // class used to create a command
+        'git_executable'  => '/usr/bin/git'       // path of the executable on the server
+    );
 
-  /**
-   * Instanciate a new Git repository wrapper
-   *
-   * @param   array $options
-   */
-  public function __construct($dir, $debug = false, array $options = array())
-  {
-    $this->dir      = $dir;
-    $this->debug    = $debug;
-    $this->options  = array_merge($this->options, $options);
-
-    $this->checkIsValidGitRepo();
-  }
-
-  /**
-   * Get branches list
-   *
-   * @return array list of branches names
-   */
-  public function getBranches()
-  {
-    return array_filter(preg_replace('/[\s\*]/', '', explode("\n", $this->git('branch'))));
-  }
-
-  /**
-   * Get current branch
-   *
-   * @return string the current branch name
-   */
-  public function getCurrentBranch()
-  {
-    $output = $this->git('branch');
-
-    foreach(explode("\n", $this->git('branch')) as $branchLine)
+    /**
+     * Instanciate a new Git repository wrapper
+     *
+     * @param   string $dir real filesystem path of the repository
+     * @param   boolean $debug
+     * @param   array $options
+     */
+    public function __construct($dir, $debug = false, array $options = array())
     {
-      if('*' === $branchLine{0})
-      {
-        return substr($branchLine, 2);
-      }
-    }
-  }
+        $this->dir      = $dir;
+        $this->debug    = $debug;
+        $this->options  = array_merge($this->options, $options);
 
-  /**
-   * Tell if a branch exists
-   *
-   * @return  boolean true if the branch exists, false otherwise
-   */
-  public function hasBranch($branchName)
-  {
-    return in_array($branchName, $this->getBranches());
-  }
-
-  /**
-   * Check if a directory is a valid Git repository
-   */
-  public function checkIsValidGitRepo()
-  {
-    if(!is_dir($this->dir.'/.git'))
-    {
-      throw new InvalidArgumentException($this->dir.' is not a valid Git repository');
+        $this->checkIsValidGitRepo();
     }
 
-    $this->git('status');
-  }
+    /**
+     * Get branches list
+     *
+     * @return array list of branches names
+     */
+    public function getBranches()
+    {
+        return array_filter(preg_replace('/[\s\*]/', '', explode("\n", $this->git('branch'))));
+    }
 
-  /**
-   * Run any git command, like "status" or "checkout -b mybranch origin/mybranch"
-   *
-   * @throws  RuntimeException
-   * @param   string  $commandString
-   * @return  string  $output
-   */
-  public function git($commandString)
-  {
-    // clean commands that begin with "git "
-    $commandString = preg_replace('/^git\s/', '', $commandString);
+    /**
+     * Get current branch
+     *
+     * @return string the current branch name
+     */
+    public function getCurrentBranch()
+    {
+        $output = $this->git('branch');
 
-    $commandString = trim($this->options['git_executable']).' '.trim($commandString);
+        foreach(explode("\n", $this->git('branch')) as $branchLine) {
+            if('*' === $branchLine{0}) {
+                return substr($branchLine, 2);
+            }
+        }
+    }
 
-    $command = new $this->options['command_class']($this->dir, $commandString, $this->debug);
+    /**
+     * Tell if a branch exists
+     *
+     * @return  boolean true if the branch exists, false otherwise
+     */
+    public function hasBranch($branchName)
+    {
+        return in_array($branchName, $this->getBranches());
+    }
 
-    return $command->run();
-  }
+    /**
+     * Check if a directory is a valid Git repository
+     */
+    public function checkIsValidGitRepo()
+    {
+        if(!is_dir($this->dir.'/.git')) {
+            throw new InvalidArgumentException($this->dir.' is not a valid Git repository');
+        }
 
-  /**
-   * Get the repository directory
-   *
-   * @return  string  the repository directory
-   */
-  public function getDir()
-  {
-    return $this->dir;
-  }
+        $this->git('status');
+    }
+
+    /**
+     * Run any git command, like "status" or "checkout -b mybranch origin/mybranch"
+     *
+     * @throws  RuntimeException
+     * @param   string  $commandString
+     * @return  string  $output
+     */
+    public function git($commandString)
+    {
+        // clean commands that begin with "git "
+        $commandString = preg_replace('/^git\s/', '', $commandString);
+
+        $commandString = trim($this->options['git_executable']).' '.trim($commandString);
+
+        $command = new $this->options['command_class']($this->dir, $commandString, $this->debug);
+
+        return $command->run();
+    }
+
+    /**
+     * Get the repository directory
+     *
+     * @return  string  the repository directory
+     */
+    public function getDir()
+    {
+        return $this->dir;
+    }
 }
--- lib/phpGitRepoCommand.php
+++ lib/phpGitRepoCommand.php
@@ -2,57 +2,68 @@
 
 class phpGitRepoCommand
 {
-  protected $dir;
+    /**
+     * @var string Real filesystem path of the repository
+     */
+    protected $dir;
 
-  protected $commandString;
+    /**
+     * @var string Git command to run
+     */
+    protected $commandString;
 
-  protected $debug;
+    /**
+     * @var boolean Whether to enable debug mode or not
+     * When debug mode is on, commands and their output are displayed
+     */
+    protected $debug;
 
-  public function __construct($dir, $commandString, $debug)
-  {
-    $commandString = trim($commandString);
-
-    $this->dir            = $dir;
-    $this->commandString  = $commandString;
-    $this->debug          = $debug;
-  }
-
-  public function run()
-  {
-    $commandToRun = sprintf('cd %s && %s', escapeshellarg($this->dir), $this->commandString);
-
-    if($this->debug)
+    /**
+     * Instanciate a new Git command
+     *
+     * @param   string $dir real filesystem path of the repository
+     * @param   array $options
+     */
+    public function __construct($dir, $commandString, $debug)
     {
-      print $commandToRun."\n";
+        $commandString = trim($commandString);
+
+        $this->dir            = $dir;
+        $this->commandString  = $commandString;
+        $this->debug          = $debug;
     }
 
-    ob_start();
-    passthru($commandToRun, $returnVar);
-    $output = ob_get_clean();
-
-    if($this->debug)
+    public function run()
     {
-      print $output."\n";
-    }
+        $commandToRun = sprintf('cd %s && %s', escapeshellarg($this->dir), $this->commandString);
 
-    if(0 !== $returnVar)
-    {
-      // Git 1.5.x returns 1 when running "git status"
-      if(1 === $returnVar && 0 === strncmp($this->commandString, 'git status', 10))
-      {
-        // it's ok
-      }
-      else
-      {
-        throw new RuntimeException(sprintf(
-          'Command %s failed with code %s: %s',
-          $commandToRun,
-          $returnVar,
-          $output
-        ), $returnVar);
-      }
-    }
+        if($this->debug) {
+            print $commandToRun."\n";
+        }
 
-    return $output;
-  }
+        ob_start();
+        passthru($commandToRun, $returnVar);
+        $output = ob_get_clean();
+
+        if($this->debug) {
+            print $output."\n";
+        }
+
+        if(0 !== $returnVar) {
+            // Git 1.5.x returns 1 when running "git status"
+            if(1 === $returnVar && 0 === strncmp($this->commandString, 'git status', 10)) {
+                // it's ok
+            }
+            else {
+                throw new RuntimeException(sprintf(
+                    'Command %s failed with code %s: %s',
+                    $commandToRun,
+                    $returnVar,
+                    $output
+                ), $returnVar);
+            }
+        }
+
+        return $output;
+    }
 }
